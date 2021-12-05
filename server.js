da9677b1ae0db9f9228cea8f2f7bc66e40155496
bekri mehdi <bekri.mehdi@outlook.com>
2021-12-05 18:42:12 +0100

    feat:  add pagination from server

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 7000;
 const app = express();
 app.use(express.json());
 let arrayFilter = [];
+let arrayFilterForPagination = [];
 
 const testFilter = (req) => {
   let test = lieuDeTournage.features;
@@ -14,29 +15,37 @@ const testFilter = (req) => {
   });
 
   arrayFilter.push(result);
+  arrayFilterForPagination.push(result);
 };
 
 app.get("/api/macaron", (req, res) => {
-  // console.log(req.query.codePostale);
+  console.log("je suis dans macaron server");
   let min = req.query.min;
   let max = req.query.max;
   if (min === undefined && max === undefined) {
-    console.log("je suis la");
     testFilter(req.query.codePostale);
-    res.send({
-      msg: arrayFilter,
-    });
     arrayFilter = [];
   }
   if (
     (min !== undefined && Number(min) !== 0) ||
     (max !== undefined && Number(max) !== 0)
   ) {
-    console.log("je suis la error");
     res.send({
       error: "error",
     });
   }
+});
+
+app.get("/api/macaron/pagination", (req, res) => {
+  const page = req.query.page;
+  const limit = req.query.limit;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const resultPagination = arrayFilterForPagination[0].slice(
+    startIndex,
+    endIndex
+  );
+  res.send(resultPagination);
 });
 
 app.listen(5000, () => {
